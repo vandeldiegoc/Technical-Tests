9cf95f6d4738537ad0ea1a16763686715b758aa4
vandel <vandeliegoc@gmail.com>
2021-10-28 19:57:46 -0500

    create structure file and create core/conf.py

---
+++ db/sess.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from ..core.config import Settings
+ 
+from base_class import Base
+
+engine = create_engine(Settings.db_URI)
+
+
+Base.metadata.create_all(bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
