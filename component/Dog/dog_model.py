ee90ea7f8d3160cd8dd6e9aac3752cd96482d4c9
vandel <vandeliegoc@gmail.com>
2021-10-30 15:46:20 -0500

    change the model in the dog table to make the name unique

--- component/Dog/dog_model.py
+++ component/Dog/dog_model.py
@@ -8,7 +8,7 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime
 
 class Dog(Base):
     id = Column(Integer(), primary_key=True)
-    name = Column(String(), nullable=False)
+    name = Column(String(), nullable=False, unique=True)
     picture = Column(String(), nullable=False, unique=True)
     is_adopted = Column(Boolean, default=False)
     create_date = Column(DateTime, default=datetime.utcnow())
